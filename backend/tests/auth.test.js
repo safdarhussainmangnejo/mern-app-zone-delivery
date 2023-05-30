68fd03b63a0ea8e63a229337143703f2895a9a76
Safdar Hussain <safdarhussainmangnejo@gmail.com>
2023-05-31 00:07:37 +0500

    worked on testing

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -6,7 +6,7 @@ describe('Authentication Routes', () => {
 
   it('should register a new user', async () => {
     const response = await request(app)
-      .post('/api/register')
+      .post('/api/signup')
       .send({ email: 'test@gmail.com', password: 'password123' });
 
     expect(response.body).toHaveProperty('message', 'User registered');
@@ -15,7 +15,7 @@ describe('Authentication Routes', () => {
   it('should log in an existing user and return a token', async () => {
     const response = await request(app)
       .post('/api/login')
-      .send({ email: 'test@example.com', password: 'password123' });
+      .send({ email: 'test@gmail.com', password: 'password123' });
 
     expect(response.body).toHaveProperty('token');
     token = response.body.token;
